a1d6e42e97a9eb8675274a96ce56f01d8e3103aa
Michel Schlatter <michel.schlatter@gmx.net>
2022-11-06 23:10:12 +0100

    initial commit

---
+++ Sentiment/Program.cs
@@ -0,0 +1,25 @@
+﻿using VaderSharp;
+
+
+
+SentimentIntensityAnalyzer analyzer = new SentimentIntensityAnalyzer();
+Console.WriteLine("Hi! Welcome to the Sentiment Analyzer");
+
+while (true)
+{
+    Console.WriteLine("Type your text and press enter");
+    string text = Console.ReadLine() ?? "";
+    if (string.IsNullOrEmpty(text))
+    {
+        Console.WriteLine("No empty strings allowed");
+    }
+    else
+    {
+        var results = analyzer.PolarityScores(text);
+
+        Console.WriteLine("Positive score: " + results.Positive);
+        Console.WriteLine("Negative score: " + results.Negative);
+        Console.WriteLine("Neutral score: " + results.Neutral);
+        Console.WriteLine("Compound score: " + results.Compound);
+    }
+}
